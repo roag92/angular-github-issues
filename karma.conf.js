4f4650e8ddb39bcc2c59e026cb047d5b48456401
Ricardo Román <ricardo.roman.ag@gmail.com>
2018-10-21 15:16:23 -0500

    Set coverage properties.

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function(config) {
             captureConsole: false
         },
         coverageIstanbulReporter: {
-            dir: require('path').join(__dirname, 'coverage'), reports: ['html', 'lcovonly'],
+            reports: ['html', 'lcovonly'],
             fixWebpackSourcePaths: true
         },
         reporters: ['progress', 'kjhtml'],
